e1b8b17c6344ef77fcbc03cdeece60574511bda2
Anthonybltrn12 <anthonybltrn@gmail.com>
2023-06-01 00:24:10 -0500

    react practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import { useState } from 'react'
-
+import { puppyList } from './data'
 import './App.css'
 
-function App() {
-  
 
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  //  console.log(featPupId)
+  //  console.log(puppies)
+   {()=>{console.log("puppy id: ", puppy.id)}}
+
+  function clickEvent(){
+    let idElement = document.createElement('section')
+    idElement.addEventListener('click', event =>{
+      idElement.innerText = `${featPupId}`
+    })
+  }
+  {()=>{console.log("puppy id: ", puppy.id)}}
   return (
-    <>
-      <div>
+    
+      <div className='App'>
         
+        {puppies.map((puppy) => {
+          return (<p onClick ={clickEvent()} key = {puppy.id}>{puppy.name}</p>
+          );
+        })}
       </div>
     
-    </>
+    
   )
 }
 
